e763b1d62524b89c3b38d011e5759dd314125244
Satoshi <6world4trigger@gmail.com>
2024-08-24 18:30:28 +0800

    Delete InferenceEnviroment (no longer needed as it's the same as train env)

--- env.py
+++ env.py
@@ -190,69 +190,6 @@ class Environment:
         return reward, next_state
 
 
-# TODO: we might not need this actually
-class InferenceEnvironment(Environment):
-    """
-    environment used during inference, that represent the environement of the actual problem world
-    """
-
-    def __init__(self, n: int = 5, item: ItemObject | None = None, goal_location: tuple[int, int] = (4, 4)) -> None:
-        super().__init__(
-            n,
-            item,
-            goal_location=goal_location,
-        )  # note: during inference, we don't use rewards
-        self.goal_location = (self.n - 1, self.n - 1)  # Set the goal state location to (n-1, n-1)
-
-    def animate(self):
-        self.ax.clear()
-        self.ax.set_xlim(0, self.n)
-        self.ax.set_ylim(self.n, 0)
-        self.ax.set_xticks(np.arange(0, self.n + 1, 1))
-        self.ax.set_yticks(np.arange(0, self.n + 1, 1))
-        self.ax.grid(True)
-
-        # Plotting the agent, item, and goal
-        self.ax.text(
-            self.agent.location[1] + 0.5,
-            self.agent.location[0] + 0.5,
-            "A",
-            ha="center",
-            va="center",
-            fontsize=16,
-            color="blue",
-        )
-        self.ax.text(
-            self.item.location[1] + 0.5,
-            self.item.location[0] + 0.5,
-            "I",
-            ha="center",
-            va="center",
-            fontsize=16,
-            color="green",
-        )
-        self.ax.text(
-            self.goal_location[1] + 0.5,
-            self.goal_location[0] + 0.5,
-            "G",
-            ha="center",
-            va="center",
-            fontsize=16,
-            color="red",
-        )
-
-        handles = [
-            plt.Line2D([0], [0], marker="o", color="w", markerfacecolor="blue", markersize=8, label="Agent (A)"),
-            plt.Line2D([0], [0], marker="o", color="w", markerfacecolor="green", markersize=8, label="Item (I)"),
-            plt.Line2D([0], [0], marker="o", color="w", markerfacecolor="red", markersize=8, label="Goal (G)"),
-        ]
-        self.ax.legend(handles=handles, loc="center left", bbox_to_anchor=(1, 0.5))
-
-        plt.subplots_adjust(right=0.75, left=0.1)
-        self.fig.canvas.draw_idle()
-        plt.pause(0.5)  # Pause to allow visualization of the movement
-
-
 class GridObject(ABC):
     def __init__(self, location: tuple[int, int] | None = None) -> None:
         self.icon: str
