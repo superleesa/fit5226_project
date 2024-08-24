089d667306db499da8d2edb88aeae2ed15208dbe
sasa0005 <shosuke0217@gmail.com>
2024-08-24 22:14:35 +0800

    delete matrics funcitons

--- test.py
+++ test.py
@@ -20,44 +20,5 @@ def test_train_viz():
         action = agent.choose_action(possible_actions, env.get_state(), agent.qval_matrix, is_training=True)
         env.step(action)  # Each step will now trigger an animation update
 
-def calculate_manhattan_distance(start_location: tuple[int, int], goal_location: tuple[int, int]):
-    # calculate shortest distance from between the locations using manhattan distance
-    start_x, start_y = start_location
-    goal_x, goal_y = goal_location
-    distance = abs(start_x - goal_x) + abs(start_y - goal_y)
-    return distance
-
-def calculate_metrics_score(shortest_distance, distance):
-    # calculate the proportion the distance (q-learning / shortest distance)
-    return distance/shortest_distance
-
-def performance_test(num_of_test = 10, n = 5):
-    # it conducs performance test for the num_of_test times, and returns the average of the proportion
-
-    # Import Agent here to avoid circular import at the top level
-    from agent import Agent
-    total_distance = 0
-    for _ in num_of_test:
-        env = Environment(n) # Create the environment
-        agent = Agent()
-        env.initialize_for_new_episode()
-        start_location = env.agent.location # get the start location for agent
-        item_location = env.item.location # get intermidate location for item
-        goal_location = (n - 1, n - 1) # get goal locaiton
-        # calculate shortest distance for the given agent starting locaiton, item locaiton, and goal location
-        shortest_distance = calculate_manhattan_distance(start_location, item_location) + calculate_manhattan_distance(item_location, goal_location)
-        total_distance += calculate_metrics_score(shortest_distance, env.num_steps)
-    return total_distance/num_of_test
-
-def main():
-    from agent import Agent, Trainer
-    environment = Environment(n=5, with_animation=False) 
-    agent = Agent()
-    trainer = Trainer(agent, environment)
-    trainer.train_for_all_items()
-
-
 if __name__ == "__main__":
     # test_train_viz()
-    # performance_test()
-    main()
