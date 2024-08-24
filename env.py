bf1d9a1e571c2a81d690d41ab5f95c9c4df9d656
Felix Tay <72016329+felixtay04@users.noreply.github.com>
2024-08-24 22:40:02 +0800

    Update env.py to tackle scenario if Item pick up item

--- env.py
+++ env.py
@@ -208,14 +208,16 @@ class InferenceEnvironment(Environment):
         # Plotting the agent, item, and goal
         self.ax.text(self.agent.location[1] + 0.5, self.agent.location[0] + 0.5, 'A',
             ha='center', va='center', fontsize=16, color='blue')
-        self.ax.text(self.item.location[1] + 0.5, self.item.location[0] + 0.5, 'I',
-            ha='center', va='center', fontsize=16, color='green')
+        if self.item.location is not None:
+            self.ax.text(self.item.location[1] + 0.5, self.item.location[0] + 0.5, 'I',
+                ha='center', va='center', fontsize=16, color='green')
         self.ax.text(self.goal_location[1] + 0.5, self.goal_location[0] + 0.5, 'G',
             ha='center', va='center', fontsize=16, color='red')
 
         handles = [
         plt.Line2D([0], [0], marker='o', color='w', markerfacecolor='blue', markersize=8, label='Agent (A)'),
         plt.Line2D([0], [0], marker='o', color='w', markerfacecolor='green', markersize=8, label='Item (I)'),
+        plt.Line2D([0], [0], marker='o', color='w', markerfacecolor='purple', markersize=8, label='Agent (A) with item')
         plt.Line2D([0], [0], marker='o', color='w', markerfacecolor='red', markersize=8, label='Goal (G)')
         ]
         self.ax.legend(handles=handles, loc='center left', bbox_to_anchor=(1, 0.5))
