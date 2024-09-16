94a3f0f5e26ec1c86e22f78ed28bd5027f51c965
sasa0005 <shosuke0217@gmail.com>
2024-09-16 23:14:07 +0800

    rename file and import file name

--- src/fit5226_project/tuning.py
+++ src/fit5226_project/tuning.py
@@ -1,60 +1,62 @@
 import optuna
 import random
 import numpy as np
+
+
 from fit5226_project.agent import DQNAgent
 from fit5226_project.env import Assignment2Environment
 from fit5226_project.actions import Action
-from fit5226_project.trainer import Trainer
+from fit5226_project.train import Trainer
 
 
 def DQN_Hyperparameter_Tune(trial: optuna.Trial) -> float:
-    # Hyperparameters to optimize
-    alpha = trial.suggest_uniform('alpha', 0.995, 0.999)
-    discount_rate = trial.suggest_uniform('discount_rate', 0.95, 0.975)
-    epsilon_decay = trial.suggest_uniform('epsilon_decay', 0.975, 0.995)
-    replay_memory_size = trial.suggest_int('replay_memory_size', 1000, 5000)
-    batch_size = trial.suggest_categorical('batch_size', [16, 32, 64, 128, 256])
-    update_target_steps = trial.suggest_int('update_target_steps', 500, 1000)
-    time_penalty = trial.suggest_int('time_penalty', -10, -1)
-    item_state_reward = trial.suggest_int('item_state_reward', 100, 400)
-    goal_state_reward = trial.suggest_int('goal_state_reward', 300, 600)
+        # Hyperparameters to optimize
+        alpha = trial.suggest_uniform('alpha', 0.995, 0.999)
+        discount_rate = trial.suggest_uniform('discount_rate', 0.95, 0.975)
+        epsilon_decay = trial.suggest_uniform('epsilon_decay', 0.975, 0.995)
+        replay_memory_size = trial.suggest_int('replay_memory_size', 1000, 5000)
+        batch_size = trial.suggest_categorical('batch_size', [16, 32, 64, 128, 256])
+        update_target_steps = trial.suggest_int('update_target_steps', 500, 1000)
+        time_penalty = trial.suggest_int('time_penalty', -10, -1)
+        item_state_reward = trial.suggest_int('item_state_reward', 100, 400)
+        goal_state_reward = trial.suggest_int('goal_state_reward', 300, 600)
 
-    tune_env = Assignment2Environment(
-      n=4,  # Grid size
-      time_penalty=time_penalty,
-      item_state_reward=item_state_reward,
-      goal_state_reward=goal_state_reward,
-      direction_reward_multiplier=1,
-      with_animation=False 
-    ) 
-    tune_agent = DQNAgent(
-      statespace_size=11,
-      action_space_size=len(Action),
-      alpha=alpha,
-      discount_rate=discount_rate,
-      epsilon=1.0, 
-      epsilon_decay=epsilon_decay,
-      epsilon_min=0.1,
-      replay_memory_size=replay_memory_size,
-      batch_size=batch_size,
-      update_target_steps=update_target_steps
-    )
+        tune_env = Assignment2Environment(
+        n=4,  # Grid size
+        time_penalty=time_penalty,
+        item_state_reward=item_state_reward,
+        goal_state_reward=goal_state_reward,
+        direction_reward_multiplier=1,
+        with_animation=False 
+        ) 
+        tune_agent = DQNAgent(
+        statespace_size=11,
+        action_space_size=len(Action),
+        alpha=alpha,
+        discount_rate=discount_rate,
+        epsilon=1.0, 
+        epsilon_decay=epsilon_decay,
+        epsilon_min=0.1,
+        replay_memory_size=replay_memory_size,
+        batch_size=batch_size,
+        update_target_steps=update_target_steps
+        )
 
-    num_episodes = 200
-    tune_trainer = Trainer(tune_agent, tune_env)
+        num_episodes = 200
+        tune_trainer = Trainer(tune_agent, tune_env)
 
-    # Initialize the environment for a new episode
-    tune_env.initialize_for_new_episode()
-    # Store the current environment reference to use throughout the episode
-    tune_env.current_sub_environment = tune_env.current_sub_environment
+        # Initialize the environment for a new episode
+        tune_env.initialize_for_new_episode()
+        # Store the current environment reference to use throughout the episode
+        tune_env.current_sub_environment = tune_env.current_sub_environment
 
-    current_state = tune_env.get_state()  # Get state from current sub-environment
-    done = False
-    total_reward = 0  # Track total reward for the episode
-    step_count = 0  # Initialize step counter
-        
-    for episode in range(num_episodes):
-        print(f"Starting Episode {episode + 1}")
+        current_state = tune_env.get_state()  # Get state from current sub-environment
+        done = False
+        total_reward = 0  # Track total reward for the episode
+        step_count = 0  # Initialize step counter
+            
+        for episode in range(num_episodes):
+            print(f"Starting Episode {episode + 1}")
 
 
         while not done: # Truncate episode after 40 steps
