0b54bfece617b1b403a16b7f0e713900c2be2750
Satoshi <6world4trigger@gmail.com>
2024-09-17 23:00:50 +0800

    Increase num_episodes for all-sub-env training

--- src/fit5226_project/evaluation.py
+++ src/fit5226_project/evaluation.py
@@ -20,7 +20,7 @@ class Evaluation:
         Trains DQN agent in the environment and save the states.
         """
         trainer = Trainer(self.dqn_agent, self.dqn_envs, with_log=True)
-        trainer.train(num_episodes=110)
+        trainer.train(num_episodes=500)
         self.dqn_agent.save_state("trained_dqn_agent_2.pth")
 
     def load_trained_dqn(self, path: str):
