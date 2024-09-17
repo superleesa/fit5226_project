c8e1121f867b65f7a750cfb8966ad48c91b1271f
Satoshi <6world4trigger@gmail.com>
2024-09-17 13:09:11 +0800

    Add PrioritizedExperienceBuffer

--- src/fit5226_project/replay_buffers.py
+++ src/fit5226_project/replay_buffers.py
@@ -40,3 +40,49 @@ class ReplayBuffer(BaseReplayBuffer):
             done_batch,
         )
 
+
+class PrioritizedExperienceBuffer(BaseReplayBuffer):
+    def __init__(self, max_size: int, alpha: float = 0.6):
+        """
+        alpha for prioritization
+        """
+        super().__init__(max_size)
+        self.alpha = alpha
+        self.priorities = np.ones(max_size, dtype=np.float32)
+        
+        self.sampled_indices: np.ndarray[int] | None = None
+
+    def remember(self, experience: Experience) -> None:
+        if len(self.buffer) >= self.max_size:
+            self.buffer.popleft()
+            self.priorities[:-1] = self.priorities[1:]
+            
+        self.buffer.append(experience)
+        max_priority = self.priorities.max() if self.buffer else 1.0  # max to ensure newly added experience has the highest priority
+        self.priorities[len(self.buffer) - 1] = max_priority
+
+    def sample_batch(self, batch_size: int) -> tuple[list[np.ndarray], list[int], list[float], list[np.ndarray], list[bool]]:
+        """
+        Sample a batch of experiences from the replay memory.
+        """
+        priorities = self.priorities[: len(self.buffer)]
+        probabilities = priorities ** self.alpha
+        probabilities /= probabilities.sum()
+
+        self.sampled_indices = np.random.choice(len(self.buffer), batch_size, p=probabilities)
+        samples = [self.buffer[idx] for idx in self.sampled_indices]
+        state_batch, action_batch, reward_batch, next_state_batch, done_batch = zip(*samples)
+        return (
+            state_batch,
+            action_batch,
+            reward_batch,
+            next_state_batch,
+            done_batch,
+        )
+
+    def update_priorities(self, priorities: np.ndarray) -> None:
+        if self.sampled_indices is None:
+            raise ValueError("You need to sample a batch before updating priorities")
+        for idx, priority in zip(self.sampled_indices, priorities):
+            self.priorities[idx] = priority
+        self.sampled_indices = None
