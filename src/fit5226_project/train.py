9de8869fd084eeb40286349330a3b22f4615e5e1
Satoshi <6world4trigger@gmail.com>
2024-09-17 22:58:19 +0800

    Ensure plot is closed after each call to visualize_sample_episode

--- src/fit5226_project/train.py
+++ src/fit5226_project/train.py
@@ -129,10 +129,7 @@ class Trainer:
             prev_state = current_state
             current_state = next_state
         
-        try:
-            plt.close(sample_env.fig)  # type: ignore
-        except AttributeError:
-            pass
+        plt.close('all')
 
     def validate(self, current_episode_index: int):
         calulated_scores = []
