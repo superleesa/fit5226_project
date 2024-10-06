e9ae209301c69bb9f7db261162ea19db504c7bd9
Satoshi <6world4trigger@gmail.com>
2024-10-07 03:52:51 +0800

    update finetuning code to use configs

--- scripts/finetune.py
+++ scripts/finetune.py
@@ -1,24 +1,26 @@
-from typing import Any
-
+import yaml
 from fire import Fire
 
-from fit5226_project.train import Trainer
-from fit5226_project.env import Assignment2Environment
 from fit5226_project.agent import DQNAgent
+from fit5226_project.env import Assignment2Environment
+from fit5226_project.train import Trainer
 
 
-def train(base_model_path: str | None = None, **agent_params: dict[str, Any]):
-    dqn_envs = Assignment2Environment(n=4, with_animation=False)
-    dqn_agent = DQNAgent(with_log=True, **agent_params)
+def train(config_path: str, base_model_path: str | None = None):
+    with open(config_path, "r") as file:
+        config = yaml.safe_load(file)
+    
+    dqn_env = Assignment2Environment(n=4, **config["env"])
+    dqn_agent = DQNAgent(**config["agent"])
     if base_model_path:
         dqn_agent.load_state(base_model_path)
     
     trainer = Trainer(
         agent=dqn_agent,
-        environment=dqn_envs,
-        with_log=True,
+        environment=dqn_env,
+        **config["trainer"]
     )
-    trainer.train(num_episodes=250)
+    trainer.train()
 
 
 if __name__ == "__main__":
