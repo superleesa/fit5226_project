1611c07d57c997f974a32c4676f5c4b2bdf52a70
Satoshi <6world4trigger@gmail.com>
2024-10-07 03:52:20 +0800

    rewrite evaluate.py using trainer.validate and trainer.visualize_sample_episode

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -1,16 +1,24 @@
+import yaml
 from fire import Fire
 
-from fit5226_project.evaluation import Evaluation
+from fit5226_project.env import Assignment2Environment
+from fit5226_project.agent import DQNAgent
+from fit5226_project.train import Trainer
 
 
-def evaluate(model_path: str):
-    evaluation = Evaluation(n=4)
-    evaluation.load_trained_dqn(model_path)
+def evaluate(model_path: str, config_path: str, num_visualizations: int = 3):
+    with open(config_path, "r") as file:
+        config = yaml.safe_load(file)
+    agent = DQNAgent(**config["agent"])
+    agent.load_state(model_path)
+    env = Assignment2Environment(n=4, **config["env"])
+    trainer = Trainer(agent, env, **config["trainer"])
     
-    average_score = evaluation.dqn_performance_test()
-    print(f"Average performance score (1 is the best): {average_score:.4f}")
-    
-    evaluation.visualize_dqn()
+    score, goal_reached_percentage, average_reward = trainer.validate(is_eval=True)
+    print("score: ", score)
+    print("goal_reached_percentage: ", goal_reached_percentage)
+    print("average_reward: ", average_reward)
+    trainer.visualize_sample_episode(num_visualizations)
 
 
 if __name__ == "__main__":
